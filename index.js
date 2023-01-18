7d1f4841afa9674e06cf90751ed8a5f906b4e2ec
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2023-01-18 23:32:06 +0100

    shopping cart

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+let books = [];
+let cart = [];
+
 const getBooks = async () => {
   try {
     let response = await fetch("https://striveschool-api.herokuapp.com/books");
 
-    let books = await response.json();
-    console.log(books);
+    books = await response.json();
+    // console.log(books);
     showBooks(books);
   } catch (error) {
     console.log("Error", error);
@@ -19,8 +22,10 @@ const showBooks = (books) => {
   let bookArray = books;
   bookArray.forEach((book) => {
     cardRow.innerHTML += `
-    <div class="col-md-3" id= "asin${book.asin}">
+    <div class="col-md-3">
+        <div class="d-none asin">${book.asin}</div>
         <div class="card mb-4 shadow-sm rounded text-center">
+            
             <img
                 src="${book.img}"
                 alt="animal picture"
@@ -30,12 +35,12 @@ const showBooks = (books) => {
                 preserveAspectRatio="xMidYMid slice"
             />
             <div class="card-header">
-                <h5>${book.title}</h5>
+                <h5 class="bookTitle">${book.title}</h5>
             </div>
             <div class="card-body">
                 <p class="card-text">
-                Category: ${book.category} <br>
-                Price: ${book.price}
+                Category: ${book.category}</p>
+                <p class="bookPrice">Price: $<span>${book.price}</span>
                 </p>
                 <div
                 class="d-flex justify-content-between align-items-center"
@@ -44,7 +49,7 @@ const showBooks = (books) => {
                         <button
                         type="button"
                         class="btn btn-sm btn-outline-secondary"
-                        onclick="addToCart(${book.asin})"
+                        onclick="addToCart(this)"
                         >
                         Add to Cart
                         </button>
@@ -63,12 +68,17 @@ const showBooks = (books) => {
   });
 };
 
-const addToCart = (asin) => {
-  console.log(asin);
-  let cardRow = document.querySelector("#cardRow");
-  let shoppingCart = document.querySelector("#shoppingCart");
+const addToCart = (buttonNode) => {
+  let cardToAddToCart = buttonNode.closest(".card");
+  let price = cardToAddToCart.querySelector(".bookPrice span").innerText;
+  let title = cardToAddToCart.querySelector(".bookTitle").innerText;
+  cardToAddToCart.classList.add("inCart");
+  let shoppingCartNode = document.querySelector("#shoppingCart");
+  shoppingCartNode.innerHTML += `<li class="dropdown-item">${title}<span class="text-success">${price}</span></li>`;
 
-  let cardToAdd = document.querySelector(`#asin${asin}`);
-  cardRow.removeChild(cardToAdd);
-  shoppingCart.appendChild(cardToAdd);
+  //   let cardRow = document.querySelector("#cardRow");
+  //   let shoppingCart = document.querySelector("#shoppingCart");
+  //   let cardToAdd = document.querySelector(`#asin${asin}`);
+  //   cardRow.removeChild(cardToAdd);
+  //   shoppingCart.appendChild(cardToAdd);
 };
